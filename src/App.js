6bfb0b26958f14cab48592c023e468726e80d3ee
MMufuT <118084878+MMufuT@users.noreply.github.com>
2023-12-04 21:18:42 -0500

    finished the initial input asking user about themselves

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+import React, { useState } from 'react';
+import ThreeBackground from './components/ThreeBackground';
+import Popup from './components/Popup';
 
 function App() {
+  const [name, setName] = useState('');
+  const [animal, setAnimal] = useState('');
+  const [fact, setFact] = useState('');
+  const [currentPopup, setCurrentPopup] = useState(1);
+
+  const handleConfirm = () => {
+    if (currentPopup < 3) {
+      setTimeout(() => {
+        setCurrentPopup(currentPopup + 1);
+      }, 500);
+    } else {
+      // All popups complete, proceed to 'About Me' page logic
+    }
+  };
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+      <ThreeBackground />
+    <div className="container vh-100" style={{ border: '0px solid blue' }}>
+      <div className="row d-flex h-100 justify-content-center align-content-center" style={{ border: '0px solid red' }}>
+        <div className="justify-content-center align-items-end d-flex" style={{ border: '0px solid green' }}>
+          <Popup
+            title="What's your name?"
+            isOpen={currentPopup === 1}
+            onClose={() => setCurrentPopup(0)}
+            onConfirm={handleConfirm}>
+            <input value={name} onChange={(e) => setName(e.target.value)} />
+          </Popup>
+
+          <Popup
+            title="What's your favorite animal?"
+            isOpen={currentPopup === 2}
+            onClose={() => setCurrentPopup(1)}
+            onConfirm={handleConfirm}>
+            <input value={animal} onChange={(e) => setAnimal(e.target.value)} />
+          </Popup>
+
+          <Popup
+            title="What's a fun fact about yourself?"
+            isOpen={currentPopup === 3}
+            onClose={() => setCurrentPopup(2)}
+            onConfirm={handleConfirm}>
+            <input value={fact} onChange={(e) => setFact(e.target.value)} />
+          </Popup>
+
+          {/* Render About Me page here based on the state */}
+        </div>
+      </div>
     </div>
+
+
+    </>
   );
 }
 
